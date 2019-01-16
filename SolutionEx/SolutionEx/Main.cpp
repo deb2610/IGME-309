83b6096fa0577b636aa68ff13a22032680b408ea
deb2610 <deb2610@rit.edu>
2019-01-16 12:39:25 -0500

    Set up example solution E01

---
+++ SolutionEx/SolutionEx/Main.cpp
@@ -0,0 +1,15 @@
+/*-------------------------------------------------------------------------------------------------- 
+Declan E. Behan
+DSA 2 Section 2
+
+This is the example solution!
+--------------------------------------------------------------------------------------------------*/ 
+#include "Main.h"
+
+int main() {
+	std::cout << "Hello World\n";
+	//Ending the program 
+	std::cout << "Press enter to finish"; 
+	getchar(); 
+	return 0; 
+}
