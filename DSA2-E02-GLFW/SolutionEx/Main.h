f9cda0ec4f924e015c3ac92d394a1ee7870b9c73
Unknown <deb2610@g.rit.edu>
2019-01-23 20:38:18 -0500

    done e02

---
+++ DSA2-E02-GLFW/SolutionEx/Main.h
@@ -0,0 +1,22 @@
+//#pragma once
+/*-------------------------------------------------------------------------------------------------- 
+Declan E. Behan
+DSA 2 Section 2
+
+This is the example solution!
+
+This project was generated in 2019
+--------------------------------------------------------------------------------------------------*/ 
+#ifndef _MAIN_H 
+#define _MAIN_H
+
+#include <iostream>
+//Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include "GL\glew.h"
+#include "GLFW\glfw3.h"
+#include "GLM\glm.hpp"
+#include "OpenGL-Tutorials\shader.hpp"
+
+#endif //_MAIN_H
